32e2aa2c73c5223f70b9781e0cc0ba0b6f57bfdb
Galvandi <andreas_schoch@outlook.com>
2019-11-16 23:05:29 +0100

    added list tags endpoint

--- app/core/admin.py
+++ app/core/admin.py
@@ -22,3 +22,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)  # no need to specify admin second param, it falls back to default one
