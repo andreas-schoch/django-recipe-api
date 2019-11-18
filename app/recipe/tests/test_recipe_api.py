091bb9d88bda296a9a624ae9c8951a799c1821e1
Galvandi <andreas_schoch@outlook.com>
2019-11-18 19:18:41 +0100

    added recipe filtering

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -203,6 +203,48 @@ class PrivateIngredientsApiTests(TestCase):
         tags = recipe.tags.all()
         self.assertEqual(len(tags), 0)
 
+    def test_filter_recipes_by_tags(self):
+        """Test returning recipes with specific tags"""
+        recipe1 = sample_recipe(user=self.user, title='bla1')
+        recipe2 = sample_recipe(user=self.user, title='bla2')
+        tag1 = sample_tag(user=self.user, name='tag1')
+        tag2 = sample_tag(user=self.user, name='tag2')
+        recipe1.tags.add(tag1)
+        recipe2.tags.add(tag2)
+        recipe3 = sample_recipe(user=self.user, title='bla3')
+
+        # Adding a payload to a GET request should return first two recipes in this case
+        res = self.client.get(RECIPES_URL, {'tags': f'{tag1.id}, {tag2.id}'})
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
+
+    def test_filter_recipes_by_ingredients(self):
+        """Test returning recipes with specific tags"""
+        recipe1 = sample_recipe(user=self.user, title='bla1')
+        recipe2 = sample_recipe(user=self.user, title='bla2')
+        ingredient1 = sample_ingredient(user=self.user, name='ing1')
+        ingredient2 = sample_ingredient(user=self.user, name='ing2')
+        recipe1.ingredients.add(ingredient1)
+        recipe2.ingredients.add(ingredient2)
+        recipe3 = sample_recipe(user=self.user, title='bla3')
+
+        # Adding a payload to a GET request should return first two recipes in this case
+        res = self.client.get(RECIPES_URL, {'ingredients': f'{ingredient1.id}, {ingredient2.id}'})
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
+
 
 class RecipeImageUploadTests(TestCase):
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,9 +43,28 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def _params_to_ints(self, query_string):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in query_string.split(',')]
+
     def get_queryset(self):
         """Retrieve the recipes for authenticated user"""
-        return self.queryset.filter(user=self.request.user)
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+
+        # filter relevant tags
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids).distinct()  # distinct prevents duplicates
+
+        # filter relevant ingredients on already tag filtered queryset
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids).distinct()
+
+        # return filtered queryset (note that )
+        return queryset.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         """create a new recipe"""
